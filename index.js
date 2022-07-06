f53f49b55620981d2861901259a18aab6a3ddfd5
entryone <a.i.kulinich@gmail.com>
2022-07-07 00:16:52 +0300

    1 file modified

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 import React, { useState } from 'react'
 import { render } from 'react-dom'
-import { AgGridReact } from 'ag-grid-react'
-import { getColumnsDef, getRowData, response } from './response'
-import * as request from './request'
 import { RequestTable } from './RequestTable'
 import { requestMock } from './requestMock'
 import { ResponseTable } from './ResponseTable'
@@ -12,8 +9,6 @@ import 'ag-grid-community/dist/styles/ag-grid.css'
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css'
 
 const App = () => {
-  const [rowData] = useState(getRowData(response)); 
-  const [columnDefs] = useState(getColumnsDef(response));
 
   return (
     <div className="ag-theme-alpine" style={{ height: 400, width: '100%' }}>
