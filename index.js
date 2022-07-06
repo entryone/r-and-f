d6a3fdfda0cec700a737c181969eb0cacb2bf69b
entryone <a.i.kulinich@gmail.com>
2022-07-06 12:05:35 +0300

    2 files modified

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
 import { render } from 'react-dom';
 import { AgGridReact } from 'ag-grid-react';
-import { getColumnsDef, response } from './mock';
+import { getColumnsDef, getRowData, response } from './mock';
 
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 const App = () => {
-  const [rowData] = useState([
+  /*const [rowData] = useState([
     { make: 'Toyota', model: 'Celica', price: 35000 },
     { make: 'Ford', model: 'Mondeo', price: 32000 },
     { make: 'Porsche', model: 'Boxter', price: 72000 },
-  ]);
+  ]); */
 
   /*const [columnDefs] = useState([
     { field: 'make' },
@@ -19,8 +19,10 @@ const App = () => {
     { field: 'price' },
   ]);*/
 
+  const rowData = getRowData(response);
   const columnDefs = getColumnsDef(response);
   console.log('columnDefs', columnDefs)
+  console.log('rowData', rowData)
 
   return (
     <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
--- mock.js
+++ mock.js
@@ -1,45 +1,57 @@
 export const response = {
   rows: [
-      {uid: "12abcd56", name: {mode: 'auto', auto: "rows.1"}},
-      {uid: "23abcd56", name: {mode: 'auto', auto: "rows.2"}},
-      {uid: "34abcd56", name: {mode: 'auto', auto: "rows.3"}}
+    { uid: '12abcd56', name: { mode: 'auto', auto: 'rows.1' } },
+    { uid: '23abcd57', name: { mode: 'auto', auto: 'rows.2' } },
+    { uid: '34abcd58', name: { mode: 'auto', auto: 'rows.3' } },
   ],
   columns: [
-      {uid: '5060', name: {mode: 'auto', auto: "Shed.1"}},
-      {uid: '6070', name: {mode: 'auto', auto: "Shed.2"}},
-      {uid: '6070', name: {mode: 'auto', auto: "Shed.3"}},
-      {uid: '6070', name: {mode: 'auto', auto: "Shed.4"}}
-      ],
+    { uid: '5060', name: { mode: 'auto', auto: 'Shed.1' } },
+    { uid: '6070', name: { mode: 'auto', auto: 'Shed.2' } },
+    { uid: '6071', name: { mode: 'auto', auto: 'Shed.3' } },
+    { uid: '6072', name: { mode: 'auto', auto: 'Shed.4' } },
+  ],
   sections: [
-      {data: [
-        [{v: 10}, {v: 20}, {v:30}, {v:40, c: "lb"}],
-        [{v: 100}, {v: 200}, {v:300}, {v:401}],
-        [{v: 10.8}, {v: 20.9}, {v:30.3}, {v:40.1}]
-      ]}
-  ]
-}
+    {
+      data: [
+        [{ v: 10 }, { v: 20 }, { v: 30 }, { v: 40, c: 'lb' }],
+        [{ v: 100 }, { v: 200 }, { v: 300 }, { v: 401 }],
+        [{ v: 10.8 }, { v: 20.9 }, { v: 30.3 }, { v: 40.1 }],
+      ],
+    },
+  ],
+};
 
+export const getRows = (data) => {
+  return data.rows;
+};
 
-export const getRows = data => {
-  return data.rows
-}
+export const getColumns = (data) => {
+  return data.columns;
+};
 
-export const getColumns = data => {
-  return data.columns
-}
+export const getData = (data, sectionIndex) => {
+  return data.sections[sectionIndex].data;
+};
 
-export const getColumnName = column => {
-  const name = column.name
-  return name[name.mode]
-}
+export const getColumnName = (column) => {
+  const name = column.name;
+  return name[name.mode];
+};
 
-export const getColumnsDef = data => {
-  const columns = getColumns(data)
-  return columns.map(column => {
+export const getColumnsDef = (data) => {
+  const columns = getColumns(data);
+  return columns.map((column) => {
     return {
-      field: getColumnName(column),
+      field: 'v',
       colId: column.uid,
-      headerName: getColumnName(column)
-    }
+      headerName: getColumnName(column),
+    };
+  });
+};
+
+export const getRowData = data => {
+  const sectionData = getData(data, 0)
+  return sectionData.map(dataRow => {
+    return {v: '5060', v: '5060', v: '5060'}
   })
-}
+};
