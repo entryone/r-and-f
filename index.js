ecc13d994fa9b0e8545611fb220cd96e7af36c18
entryone <a.i.kulinich@gmail.com>
2022-07-06 21:46:44 +0300

    1 file modified

--- index.js
+++ index.js
@@ -8,20 +8,14 @@ import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 const App = () => {
-  /*const [rowData] = useState([
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxter', price: 72000 },
-  ]); */
+  const [rowData] = useState(getRowData(response)); 
+  const [columnDefs] = useState(getColumnsDef(response));
 
-  /*const [columnDefs] = useState([
-    { field: 'make' },
-    { field: 'model' },
-    { field: 'price' },
-  ]);*/
+  const [rowData2] = useState(request.getRowData(request.request.rnf)); 
+  const [columnDefs2] = useState(request.getColumnsDef(request.request.rnf));
 
-  const rowData = getRowData(response);
-  const columnDefs = getColumnsDef(response);
+  //const rowData = getRowData(response);
+  //const columnDefs = getColumnsDef(response);
   //const rowData2 = request.getRowData(request.request.rnf);
   //const columnDefs2 = request.getColumnsDef(request.request.rnf);
   //console.log('columnDefs', columnDefs)
@@ -30,7 +24,6 @@ const App = () => {
   return (
     <div className="ag-theme-alpine" style={{ height: 400, width: '100%' }}>
       <AgGridReact rowData={rowData} columnDefs={columnDefs} />
-    
     </div>
   );
 };
